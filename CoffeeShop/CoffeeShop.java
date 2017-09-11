0f2934aa3d8e04825fc479375ce681ba7b842037
Alice Lien <alicelien@Alicede-MacBook-Pro.local>
2017-09-11 14:30:33 -0700

    hi

---
+++ CoffeeShop/CoffeeShop.java
@@ -0,0 +1,19 @@
+/*
+  Project name: CoffeeShop
+  Project description: this project simulates a basic coffee shop service
+  Programmer: Nerissa Lien
+  Version: Sep 11/ 2017
+*/
+import java.util.*;
+public class CoffeeShop{
+  //record order
+  System.out.println("Welcome to NotStarbucks, may I take your order?" \n + "Ex. 1 capuccino 2 hotchocolate");
+  Scanner input= new Scanner(Systme.in);
+  String order=nextLine();
+  System.out.println("Can I have a name for that?");
+  Scanner input2= new Scanner(System.in);
+  String name= input2.nextLine();
+  //print the final confirmation
+  System.out.println("One moment please...");
+  System.out.println(name + "! Your "+ order + " is ready.");
+}
