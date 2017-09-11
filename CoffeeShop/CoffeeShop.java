eebfd2e01476757530f31c4d62f1be038708da4c
Alice Lien <alicelien@Alicede-MacBook-Pro.local>
2017-09-11 15:28:31 -0700

    hi

--- CoffeeShop/CoffeeShop.java
+++ CoffeeShop/CoffeeShop.java
@@ -6,14 +6,17 @@
 */
 import java.util.*;
 public class CoffeeShop{
+public static void main(String[] args) {
   //record order
-  System.out.println("Welcome to NotStarbucks, may I take your order?" \n + "Ex. 1 capuccino 2 hotchocolate");
-  Scanner input= new Scanner(Systme.in);
-  String order=nextLine();
+  System.out.println("Welcome to NotStarbucks, may I take your order? \n Ex. 1 capuccino 2 hotchocolate");
+  Scanner input= new Scanner(System.in);
+  String order=input.nextLine();
   System.out.println("Can I have a name for that?");
   Scanner input2= new Scanner(System.in);
   String name= input2.nextLine();
   //print the final confirmation
   System.out.println("One moment please...");
   System.out.println(name + "! Your "+ order + " is ready.");
+}
+
 }
