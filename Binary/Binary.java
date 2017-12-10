5648363f8b0dae7e5af56982a67b4344c370ffae
Alice Lien <alicelien@Alicede-MacBook-Pro.local>
2017-12-09 17:16:37 -0800

    12/9

--- Binary/Binary.java
+++ Binary/Binary.java
@@ -1,5 +1,11 @@
 import java.util.*;
-import java.lang.*;
+import java.lang.*; //Math
+/*
+  Program name: Binary.java
+  Program description: a calculator for binary numbers
+  Programmer: Nerissa Lien
+  Version: 12/ 9/ 17
+*/
 public class Binary{
   static int[] bin1=new int[8];
   static int[] bin2=new int[8];
@@ -21,8 +27,8 @@ public class Binary{
     Scanner input3= new Scanner(System.in);
     char operator= input3.next().charAt(0);
     */
-    //  System.out.println(Arrays.toString(test.addition(bin1,bin2)));
-      System.out.println(Arrays.toString(test.substraction()));
+    System.out.println("addition"+Arrays.toString(test.addition(bin1,bin2)));
+      System.out.println("multiply"+Arrays.toString(test.multiply()));
   /*
   if(operator=='a' || operator=='A'){
     System.out.println(Arrays.toString(test.addition()));
@@ -35,6 +41,7 @@ public class Binary{
 
   }
 
+  //converting the String input to integer array
   public void number(String a, String b){
     for(int i=0; i<a.length(); i++){
       bin1[8-a.length()+i]=(int)a.charAt(i)-'0';
@@ -44,6 +51,7 @@ public class Binary{
     }
   }
 
+  //Method for adding the two binary inputs
   public int[] addition(int[] a, int[] b){
     boolean overflow=false;
     for(int i=7; i>=0; i--){
@@ -68,7 +76,7 @@ public class Binary{
     return result;
   }
 
-
+  //Method for minusing the two binary inputs
   public int[] substraction(){
     int[] negBin2= new int[8];
     int[] complement= new int[] {0,0,0,0,0,0,0,1};
@@ -82,26 +90,26 @@ public class Binary{
       }
     }
     negBin2=addition(negBin2,complement);
-    System.out.println(Arrays.toString(negBin2));
     return addition(bin1, negBin2);
   }
 
+  //Method for multiplying the two binary inputs
   public int[] multiply(){
+    result= new int[]{0,0,0,0,0,0,0,0};
     int mut=0;
-    for(int i=0; i<8 ; i++){
-      mut+=bin2[i]*Math.pow(2,i);
+    int[] newresult= new int[8];
+    //find the multiplier
+    for(int i=7; i>=0 ; i--){
+      mut+=bin2[i]*(int)Math.pow(2,7-i);
     }
-    System.out.println(mut);
-    result=bins1;
-    for(int i=1; i< mut; i++){
-      result=addition(result, bin1);
+    //multiplication is just a loop of addition
+    //be aware array is a object "copyOf" prevent the value being overwrite
+    for(int i=1; i<=mut; i++){
+      newresult=Arrays.copyOf(result,8);
+      System.out.println(i+Arrays.toString(result));
+      result=addition(newresult, bin1);
+      System.out.println(i+Arrays.toString(result));
     }
-    //System.out.println(Arrays.toString(result));
     return result;
-  /*  for(int i=0; i<8; i++){
-      for(int j=0; j<8; j++){
-
-      }
-    }*/
   }
 }
