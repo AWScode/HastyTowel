e77d3cfa9454426a229c0d93f97a5c2238724aae
Alice Lien <alicelien@Alicede-MacBook-Pro.local>
2018-03-05 20:11:02 -0800

    Sign class

--- CS IA/Sign.java
+++ CS IA/Sign.java
@@ -1,24 +1,62 @@
+import java.io.*;
 import java.util.*;
 
+
 public class Sign{
-  private HashMap<Integer, Student> studentList;
+  //holds the list of students who are late
   private ArrayList<String> lateList;
+  //holds each student's file and can be searched by ID number
+  private HashMap<Integer, Student> stuList;
 
   public static void main(String[] args){
-
+    Sign sign=new Sign();
+    sign.signIn(3451141);
+    sign.signIn(12345678);
+    sign.signIn(1234560);
+    sign.endSign();
+    System.out.println(sign.getLateList().toString());
   }
 
+
+  //Class Sign constructor
+  public Sign(){
+		try{
+      stuList=new HashMap<>();
+      lateList=new ArrayList<>();
+  		Scanner input= new Scanner(new File ("student.txt"));
+  			while(input.hasNext()){
+  				Student stu = new Student(input.nextInt(), input.next(), input.next());
+  				stuList.put(stu.getId(), stu);
+  			}
+  			input.close();
+  		}
+  		catch(FileNotFoundException e){
+  			System.out.println("Do not find file.");
+  		}
+  	}
+
+  //Method SignIn takes in student ID number to reecord that the student is present in the student's file
   public void signIn(int studentId){
-    for (Integer id : studentList.keySet()){
-      if(studentId==id){
-        Student a= studentList.get(id);
-        a.setLate(false);
+    boolean valid=false;
+      for(Integer id : stuList.keySet()){
+        if(studentId==id){
+          Student a= stuList.get(id);
+          a.setLate(false);
+          valid=true;
+        }
       }
+    if(valid){
+        valid=false;
+    }
+    else{
+      System.out.println("Invalid number");
     }
   }
+
+  //Method endSign stops students from beinng allowed to record themselves present from the late list
   public void endSign(){
-    for (Integer id : studentList.keySet()){
-        Student a= studentList.get(id);
+    for (Integer id : stuList.keySet()){
+        Student a= stuList.get(id);
         if(a.getLate()==true){
           lateList.add(a.getName());
           a.addLateT();
@@ -27,10 +65,12 @@ public class Sign{
     }
   }
 
+  //Method getLateList returns the list of students who are late
   public ArrayList getLateList(){
     return lateList;
   }
 
+  //Method clearLateList earase the late list of that day
   public void clearLateList(){
     lateList.clear();
   }
